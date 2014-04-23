f468ebb3801acbcc71421b133242485987e95ce7
Brendan Griffen <brendan.f.griffen@gmail.com>
2014-04-23 16:20:45 -0400

    update

--- minchange.py
+++ minchange.py
@@ -3,13 +3,14 @@ import multiprocessing
 import numpy as np
 import time
 
-
 # get list of coins
-totcoins = 20
+ncores = 8
+totcoins = 10 # number of coins you would like
+ndenom = 3 # number of coin denominations
 coins = np.arange(1,totcoins+1)
 
 # create combinations
-combos = itertools.combinations(coins,5)
+combos = itertools.combinations(coins,ndenom)
 
 def parallel_getminchange(index, V, C):
     m, n = len(V)+1, C+1
@@ -22,36 +23,6 @@ def parallel_getminchange(index, V, C):
             table[i][j] = min(table[i-1][j], 1 + aC)
     return (index, table[m-1][n-1])
 
-
-start = time.time()
-
-indices = []
-results = []
-
-def store_result(args):
-    index, result = args
-    indices.append(index)
-    results.append(result)
-
-# Summer time
-pool = multiprocessing.Pool(processes=8)
-for i, combo in enumerate(combos):
-    #print i,combo
-    pool.apply_async(parallel_getminchange, args=(i, combo, totcoins), callback=store_result)
-
-# Winter time
-pool.close()
-pool.join()
-
-# Get minimum
-argmin = np.argmin(results)
-comboi, minchangecombo = indices[argmin], results[argmin]
-print comboi, minchangecombo
-elapsed = time.time() - start
-print "Parallel Elasped: %3.2f minutes" % (elapsed/60.)
-#print "Best combination:",combos[comboi]
-#print "Best combination sum:",sum(np.array(comboi))
-
 def serial_getminchange(V, C):
     m, n = len(V)+1, C+1
     table = [[0] * n for x in xrange(m)]
@@ -63,20 +34,35 @@ def serial_getminchange(V, C):
             table[i][j] = min(table[i-1][j], 1 + aC)
     return table[m-1][n-1]
 
+start = time.time()
+
+indices = []
+results = []
+
+def store_result(args):
+    index, result = args
+    indices.append(index)
+    results.append(result)
+
+pool = multiprocessing.Pool(processes=ncores)
+for i, combo in enumerate(combos):
+    pool.apply_async(parallel_getminchange, args=(i, combo, totcoins), callback=store_result)
+
+pool.close()
+pool.join()
+
+argmin = np.argmin(results)
+comboi, minchangecombo = indices[argmin], results[argmin]
+elapsed = time.time() - start
+print "Parallel Elapsed: %3.2f minutes" % (elapsed/60.)
+
 minchangecombo =100
 start = time.time()
 for i, combo in enumerate(combos):
-    print i,combo
-    print("At {0} {1}".format(i, combo))
     changecombo_i = serial_getminchange(combo,totcoins)
     if changecombo_i < minchangecombo:
         minchangecombo = changecombo_i
         comboi = combo
-        #print "Combination:",comboi
-        #print "Sum:",sum(np.array(comboi))
-#print "Found minimum combos: %3.2f hours" % (elapsed/3600.)
-#print "Best combination:",comboi
-#print "Best combination sum:",sum(np.array(comboi))
+
 elapsed = time.time() - start
-print comboi, minchangecombo
-print "Serial Elasped: %3.2f minutes" % (elapsed/60.)
+print "Serial Elapsed: %3.2f minutes" % (elapsed/60.)
